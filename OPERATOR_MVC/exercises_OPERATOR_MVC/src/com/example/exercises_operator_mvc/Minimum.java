a074c4195a8625fd0bc267cdd0ec80b977201ff7
GeraldGono <g.gono@klab.com>
2014-07-01 10:58:04 +0800

    exercises for MVC android

---
+++ OPERATOR_MVC/exercises_OPERATOR_MVC/src/com/example/exercises_operator_mvc/Minimum.java
@@ -0,0 +1,11 @@
+package com.example.exercises_operator_mvc;
+
+public class Minimum implements Operation {
+	// performs minimum operation and implements Operations Interface
+	@Override
+	public int execute(int val1, int val2) {
+		return (val1 < val2 ? val1 : val2);
+	}
+
+
+}
---
+++ OPERATOR_MVC/exercises_OPERATOR_MVC/src/com/example/exercises_operator_mvc/Operation.java
@@ -0,0 +1,8 @@
+package com.example.exercises_operator_mvc;
+
+public interface Operation {
+	// Operation interface class declaration of execute method
+	public int execute(int val1, int val2);
+
+
+}
