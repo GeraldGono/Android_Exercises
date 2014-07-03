400f6ea1e3220fb1e3b62e82bc6059a3a58a6ecc
GeraldGono <g.gono@klab.com>
2014-07-03 11:30:36 +0800

    Update Touch.java

--- BITMAP/exercises_BITMAP/src/com/example/exercises_bitmap/Touch.java
+++ BITMAP/exercises_BITMAP/src/com/example/exercises_bitmap/Touch.java
@@ -11,16 +11,16 @@ import android.widget.ImageView;
 
 public class Touch implements OnTouchListener {
 	// matrices will be used to move and zoom images
-	Matrix matrix = new Matrix();
-	Matrix savedMatrix = new Matrix();
+	private Matrix matrix = new Matrix();
+	private Matrix savedMatrix = new Matrix();
 	// declaration of 3 states of ImageView
 	static final int NONE = 0;
 	static final int DRAG = 1;
 	static final int ZOOM = 2;
 	int mode = NONE;
 
-	PointF start = new PointF();
-	PointF mid = new PointF();
+	private PointF start = new PointF();
+	private PointF mid = new PointF();
 	float oldDist = 1f;
 
 	@Override
