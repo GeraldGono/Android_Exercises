f56fe39025e46f3e96ae4d9a7077591362863b0e
GeraldGono <g.gono@klab.com>
2014-07-03 16:47:48 +0800

    volley lrucache json

---
+++ exercises_VOLLEY/exercises_VOLLEY_LRU/gen/com/example/exercises_volley_lru/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.exercises_volley_lru;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ exercises_VOLLEY/exercises_VOLLEY_LRU/src/com/example/exercises_volley_lru/Const.java
@@ -0,0 +1,10 @@
+package com.example.exercises_volley_lru;
+
+public class Const {
+
+	public static final String URL_JSON_OBJECT = "http://date.jsontest.com ";
+	public static final String URL_JSON_ARRAY = "http://api.androidhive.info/volley/person_array.json";;
+	public static final String URL_STRING_REQ = "http://echo.jsontest.com/key/value/one/two";
+	public static final String URL_IMAGE = "http://stuffpoint.com/dogs/image/307235-dogs-cute-dog.jpg";
+
+}
